16d84393d9893c23c0c1f6dcf968cc887f3135cb
jonathan <jonathan@mg.corellia.be>
2022-10-24 02:09:46 +0300

    add configuration of dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,34 @@
+import { TopBarComponent } from '../dashboard/components/top-bar/top-bar.component';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
 
-
-
+const route: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full'
+      },
+      {
+        path: 'users',
+        loadChildren: () => import("../dashboard/views/users/users.module").then(m => m.UsersModule)
+      }
+    ]
+  }
+]
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    TopBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(route)
   ]
 })
 export class DashboardModule { }
