85c3d1d9fc5c16068210e4ace918908363b9b837
jonathan <jonathan@mg.corellia.be>
2022-10-24 03:43:03 +0300

    add delivery man module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -17,6 +17,14 @@ const route: Routes = [
       {
         path: 'users',
         loadChildren: () => import("../dashboard/views/users/users.module").then(m => m.UsersModule)
+      },
+      {
+        path: 'delivery',
+        loadChildren: () => import("../dashboard/views/delivery-man/delivery-man.module").then(m => m.DeliveryManModule)
+      },
+      {
+        path: 'orders',
+        loadChildren: () => import("../dashboard/views/orders/orders.module").then(m => m.OrdersModule)
       }
     ]
   }
