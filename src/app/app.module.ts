fca3a99d30a64e062cf8a048b3d96a32fb0ec6cd
jonathan <jonathan@mg.corellia.be>
2022-10-24 08:14:14 +0300

    configure OrderState in App.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+import { OrdersState } from 'src/store/order/orders.state';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([
+      OrdersState
+    ], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production
+    }),
+    NgxsStoragePluginModule.forRoot({
+      key: [
+        OrdersState
+      ],
+      storage: StorageOption.LocalStorage
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
